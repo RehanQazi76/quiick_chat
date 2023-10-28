93aa11d195c4c89ca5c20a999f888ce5f3b619c4
RehanQazi76 <rehanqazi02@gmail.com>
2023-10-29 01:11:07 +0530

    ordered by timeStamp

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:quiick_chat/constants.dart';
 import 'package:cloud_firestore/cloud_firestore.dart  ';
+import 'package:timeago/timeago.dart' as timeago;
 
 class ChatScreen extends StatefulWidget {
   static const String id='/chat';
@@ -69,14 +70,14 @@ final user=   _auth.authStateChanges()
           children: <Widget>[
 
             StreamBuilder<QuerySnapshot>(
-              stream: _firestore.collection("messages").snapshots(),
+              stream: _firestore.collection("messages").orderBy("createdOn").snapshots(),
               builder:(context,snapshot) {
                 if(!snapshot.hasData){
                   return CircularProgressIndicator(backgroundColor: Colors.white,);
                 }
                 List <BubbleMessage> messageWidgets=[];
                 bool check;
-                final List<QueryDocumentSnapshot> lists=snapshot.data!.docs.toList() ;
+                final List<QueryDocumentSnapshot> lists=snapshot.data!.docs.reversed.toList() ;
                 for(var list in lists){
                   if(currentUser!.email.toString() !=list["sender"].toString()){
                     check=false;
@@ -84,13 +85,16 @@ final user=   _auth.authStateChanges()
                   else{
                     check=true;
                   }
-                  final messageWidget=BubbleMessage(sender: list["sender"].toString(),message: list["message"].toString(),check:check);
+                  String Time=TimeOfDay.fromDateTime(list["createdOn"].toDate()).toString();
+                  final messageWidget=BubbleMessage(sender: list["sender"].toString(),message: list["message"].toString(),createdOn: Time.substring(10,15),check:check);
+
                   messageWidgets.add(messageWidget);
                 }             
                 return Expanded(
                   child: ListView(
                     reverse: true,
-                    padding: EdgeInsets.symmetric(vertical: 10,horizontal: 10),
+                    padding: EdgeInsets.only(bottom: 15),
+                    
                     children: messageWidgets,
                   ),
                 );
@@ -116,12 +120,17 @@ final user=   _auth.authStateChanges()
                   TextButton(
                     onPressed: () {
                       messages.clear();
+                      if(message!=""){
                       print(message);
                       var data ={
                         "message":message,
                         "sender":currentUser!.email,
+                        "createdOn": DateTime.now()                        
                       };
-                      _firestore.collection("messages").add(data);
+                      print(DateTime.now());
+                      
+                      _firestore.collection("messages").add(data);}
+                      message="";
                       
                     },
                     child: Text(
@@ -140,9 +149,10 @@ final user=   _auth.authStateChanges()
 }
 
 class BubbleMessage extends StatelessWidget {
-  const BubbleMessage({this.sender,this.message,this.check});
+  const BubbleMessage({this.sender,this.message,this.createdOn,this.check});
   final sender;
   final message;
+  final createdOn;
   final check;
 
   @override
@@ -170,16 +180,31 @@ class BubbleMessage extends StatelessWidget {
                       
                             
                           ),
-                          child: Text(message,
-                          style: TextStyle(fontSize: 35,
-                          color: check? Colors.white:Colors.black,
-                          
-                          ),
+                          child: Column(
+                            crossAxisAlignment: check? CrossAxisAlignment.end:CrossAxisAlignment.start,
+                            children: [
+                              Text(message,
+                              style: TextStyle(fontSize: 35,
+                              color: check? Colors.white:Colors.black,
+                              
+                              ),
+                              ),
+                               SizedBox(height: 5,),
+                        Text(createdOn,
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: check? Colors.white:Colors.black,
+                      ),
+                      ),
+                            ],
                           ),
                           
                           ),
                       ),
-                        SizedBox(height: 15,)
+                      const SizedBox(
+                        height: 20,
+                      )
+                       
                     ],
                   );;
   }
